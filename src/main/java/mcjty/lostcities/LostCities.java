947479324df4b7d70d8f2d6c481df34616e5b3cc
AnodeCathode <shotorius_dt@hotmail.com>
2021-04-07 10:38:11 -0600

    Version bump

--- src/main/java/mcjty/lostcities/LostCities.java
+++ src/main/java/mcjty/lostcities/LostCities.java
@@ -23,7 +23,7 @@ import java.util.function.Function;
 
 public class LostCities {
     public static final String MODID = "lostcities_tfc";
-    public static final String VERSION = "12.2.66";
+    public static final String VERSION = "12.2.66.1";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
 
     @SidedProxy(clientSide="mcjty.lostcities.setup.ClientProxy", serverSide="mcjty.lostcities.setup.ServerProxy")
